b7bd3e0da2e9cbdc1bc62d04c8c85b9e91ddca42
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-02-25 09:56:49 +0200

    finsh project

--- pages/another/[id].js
+++ pages/another/[id].js
@@ -14,10 +14,34 @@ export const getStaticPaths = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { another: data },
+  };
+};
+
+const Details = ({ another }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>Name: {another.name}</h1>
+      <h3>Username: {another.username}</h3>
+      <p>Email: {another.email}</p>
+      <p>Address: {another.address.street}</p>
+      <p>Address Suite: {another.address.suite}</p>
+      <p>Address City: {another.address.city}</p>
+      <p>Address Zipcode: {another.address.zipcode}</p>
+      <p>
+        Lat: {another.address.geo.lat}, Lng: {another.address.geo.lng}
+      </p>
+      <p>Phone: {another.phone}</p>
+      <p>Website: {another.website}</p>
+      <p>Company name: {another.company.name}</p>
+      <p>Company Catch Phrse: {another.company.catchPhrse}</p>
+      <p>Campany Bs: {another.company.bs}</p>
     </div>
   );
 };
