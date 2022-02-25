5ba8a21439e0e6ae64039af8ba2d32a5c98caaf4
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-02-25 08:45:11 +0200

    Get Static Props

--- pages/another/index.js
+++ pages/another/index.js
@@ -1,8 +1,24 @@
-const Another = () => {
+import styles from "../../styles/Another.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { anothers: data },
+  };
+};
+
+const Another = ({ anothers }) => {
   return (
-    <div>
+    <section>
       <h1>Another</h1>
-    </div>
+
+      {anothers.map((another) => (
+        <a className={styles.single}>
+          <h3>{another.name}</h3>
+        </a>
+      ))}
+    </section>
   );
 };
 
