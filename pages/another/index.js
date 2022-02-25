d6d882a3c234babc15da63ab2123ca927b0753bd
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-02-25 09:12:32 +0200

    dynamic routes

--- pages/another/index.js
+++ pages/another/index.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import styles from "../../styles/Another.module.css";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
+
   return {
     props: { anothers: data },
   };
@@ -14,9 +16,11 @@ const Another = ({ anothers }) => {
       <h1>Another</h1>
 
       {anothers.map((another) => (
-        <a className={styles.single}>
-          <h3>{another.name}</h3>
-        </a>
+        <Link href={"/another/" + another.id} key={another.id}>
+          <a className={styles.single}>
+            <h3>{another.name}</h3>
+          </a>
+        </Link>
       ))}
     </section>
   );
